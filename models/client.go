f9da7db1e48a8bad2decb1f15121d3e38f685771
Hadwin <linnaiwei@gmail.com>
2020-05-03 15:35:02 +0800

    Implement a function of reading json from a client

--- models/client.go
+++ models/client.go
@@ -1,9 +1,38 @@
 package models
 
-import "github.com/gorilla/websocket"
+import (
+	"encoding/json"
+	"github.com/gorilla/websocket"
+)
 
 type Client struct {
 	id string
 	socket *websocket.Conn
 	send chan []byte
 }
+
+func NewClient() *Client {
+	return &Client{}
+}
+
+func (c *Client) Read(manager *ClientManager){
+	defer func() {
+		manager.unregister <- c
+		_ = c.socket.Close()
+	}()
+
+	for {
+		_, message, err := c.socket.ReadMessage()
+		if err != nil {
+			manager.unregister <-c
+			_ = c.socket.Close()
+			break
+		}
+		jsonMessage, _ := json.Marshal(&Message{
+			Sender:    c.id,
+			Content:   string(message),
+		})
+		manager.broadcast <- jsonMessage
+	}
+}
+
