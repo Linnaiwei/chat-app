761ee5e6c7cafc5c1def4d8b415d8c4e6ec695f3
Hadwin <linnaiwei@gmail.com>
2020-05-03 14:19:54 +0800

    Add a constructor of ClientManager

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "chat-app/models"
+
+func main () {
+	//spin up a global ClientManager
+	manager := models.NewClientManager()
+}
--- models/clientManager.go
+++ models/clientManager.go
@@ -6,3 +6,15 @@ type ClientManager struct{
 	register chan *Client
 	unregister chan *Client
 }
+
+func NewClientManager() *ClientManager {
+	return &ClientManager{
+		clients: make(map[*Client]bool),
+		broadcast: make(chan []byte),
+		register: make(chan *Client),
+		unregister: make(chan *Client),
+	}
+}
+
+
+
